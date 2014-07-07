2fdf9908a909021b63b292a603d419cc37280e1d
Agnete Moos <agms@sonderborg.dk>
2014-07-07 11:00:49 +0200

    Hide search form if user login form is visible

--- scripts/bibsdb.js
+++ scripts/bibsdb.js
@@ -9,10 +9,17 @@
 		// Hide message about reservation expiration, because we don't do that in Sonderborg
 		$(".warning:contains('This reservation is about to expire.')").hide();
 		
-		// Show topbar search box on all pages except search result pages
+		
+		
+		
+		// Show topbar search box on all pages except search result pages 
+		// Also hide if login form is visible
 		if (window.location.href.indexOf("search/") > -1) {
-					$('.js-topbar-search').css("display", "hidden");
+			$('.js-topbar-search').css("display", "hidden");
 		}
+		else if ($('.js-topbar-user').is(":visible")) {
+			$('.js-topbar-search').css("display", "hidden");
+	  } 
 		else {
 			// Display the element.
 			$('.js-topbar-search').css("display", "block");
