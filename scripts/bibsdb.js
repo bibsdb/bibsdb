564d1df6e898f45a7b7778d4da633088a6a3677c
Agnete Moos <agms@sonderborg.dk>
2014-08-18 14:38:43 +0200

    Hide the first event-list-location in event-lists if there is a second. Do this to NOT print the library-link if the location is else where. Done with javascript.

--- scripts/bibsdb.js
+++ scripts/bibsdb.js
@@ -10,7 +10,17 @@
 		$(".warning:contains('This reservation is about to expire.')").hide();
 		
 		
-		
+		// Hide the first event-list-location in lists if there is a second.
+		// Do this to NOT print the library-link if the location is else where.
+
+		$('.event-list-item').each(function(i) {
+				
+        var location = $(this).find('.event-list-location');
+        if(location.length > 1) {
+					location.first().hide();
+				}
+        
+    });
 		
 		// Show topbar search box on all pages except search result pages 
 		// Also hide if login form is visible
