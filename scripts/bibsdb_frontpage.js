9a6522acbf5aeb102b5a985d46ed6853f9419d48
Agnete Moos <agms@sonderborg.dk>
2015-01-19 13:17:22 +0100

    Added carrousel to front page and moved links to header

--- scripts/bibsdb_frontpage.js
+++ scripts/bibsdb_frontpage.js
@@ -1,37 +1,35 @@
 (function($) {
 
+   /**
+    * Attach links to front-page
+    */    
+  function attach_links_to_front_page() {
+   
+ 
 
-	function calculate_link_box_height() {
-		var windowWidth = $(window).width();
-		
-		if (windowWidth >= 1210) {
-			linkBoxHeight = 290;
-		}
-		else {
-			linkBoxHeight = 290 * windowWidth / 1210
-		}
-		return linkBoxHeight;
-	}
+    var elements = new Array();
+    elements[0] = $('<section>').addClass("bibsdb-frontpage-links");
+        
+    var link1 = $('<a>').attr("href", "http://biblioteket.sonderborg.dk/sites/default/files/sadan_soger_og_reserverer_du.pdf").attr("target", "_blank").text("Sådan søger og reserverer du");
+    var link2 = $('<a>').attr("href", "http://biblioteket.sonderborg.dk/sites/default/files/sadan_fornyer_du.pdf").attr("target", "_blank").text("Sådan fornyer du");
+    var link3 = $('<a>').attr("href", "http://biblioteket.sonderborg.dk/sites/default/files/sadan_betaler_du_gebyrer.pdf").attr("target", "_blank").text("Sådan betaler du gebyrer");
+    var link4 = $('<a>').attr("href", "http://biblioteket.sonderborg.dk/sites/default/files/sadan_gemmer_eller_sender_du_din_huskeliste.pdf").attr("target", "_blank").text("Sådan gemmer eller sender du din huskeliste");
+    
+    
+    elements[0].append($('<div>').append(link1), $('<div>').append(link2), $('<div>').append(link3));
 
-	/**
-	 * Set height on frontpage-link-box on frontpage depending on window-width
-	 */
-	function set_height_on_frontpage_link_box() {
 
-		 $('.bibsdb-frontpage-links').height(calculate_link_box_height());
-			$(window).resize(function () {
-							$('.bibsdb-frontpage-links').height(calculate_link_box_height());
-			});
-	}
+    //select element to insert into
+    $("section.search").after(elements[0]);
+  }
 
+	
 
 
 
   // When ready start the magic.
   $(document).ready(function () {
-		set_height_on_frontpage_link_box();
-
-	
+		attach_links_to_front_page();
 
 	});
 
