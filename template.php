8ed61259920116ddaaf984a924ac4cab48a4c17b
Agnete Moos <agms@sonderborg.dk>
2015-11-26 11:05:38 +0100

    Make authers full name available to News template by using function provided by RealName module

--- template.php
+++ template.php
@@ -13,18 +13,8 @@ function bibsdb_preprocess_node(&$variables){
 		// Make the publication date available. This depends on the publication_date module being installed.
 	  $variables['bibsdb_publication_date'] = format_date($node->published_at, 'ding_long_date_only');
 
-	  //Make authors full name available
-		$uid = user_load($node->uid);
-		$fullname = [];
-		$myprofile = profile2_load_by_user($uid, 'ding_staff_profile');
-		if (isset($myprofile->field_ding_staff_forename['und'][0]['value'])) {
-	    $forename = $myprofile->field_ding_staff_forename['und'][0]['value'];
-			$fullname[] = $forename;
-		}
-		if (isset($myprofile->field_ding_staff_surname['und'][0]['value'])) {
-	    $surname = $myprofile->field_ding_staff_surname['und'][0]['value'];
-			$fullname[] = $surname;
-		}
-		$variables['bibsdb_author_full_name'] = implode(" ", $fullname);
+	  // Make authors full name available using the RealName module
+	  $uid = user_load($node->uid);
+	  $variables['bibsdb_author_full_name'] = check_plain(format_username($uid));
 	}
 }
