9f56339aef70ff03d9f03450291fe08ef84bddb6
Agnete Moos <agms@sonderborg.dk>
2016-05-30 12:15:50 +0200

    Fix page styling to fit walls

--- scripts/bibsdb_other.js
+++ scripts/bibsdb_other.js
@@ -5,37 +5,45 @@
 	 * Tags show and hide
 	 * Hide page tags on search result items and items in taxonomy lists
 	 * Hide page tags on bibsdb-rowlists except special tags
-   */
-	function hide_page_taxonomy_tags() {
+	 */
+	 function hide_page_taxonomy_tags() {
 
-		$( ".field-name-field-ding-page-tags" ).hide();
-		$(".bibsdb-rowlist-tags li").hide();
-		$(".bibsdb-rowlist-tags li:contains('Adgang fra biblioteket')").show();
-		$(".bibsdb-rowlist-tags li:contains('Adgang hjemmefra')").show();
+	 	$( ".field-name-field-ding-page-tags" ).hide();
+	 	$(".bibsdb-rowlist-tags li").hide();
+	 	$(".bibsdb-rowlist-tags li:contains('Adgang fra biblioteket')").show();
+	 	$(".bibsdb-rowlist-tags li:contains('Adgang hjemmefra')").show();
 
-	}
-	
-	function bibsdb_foldable_list() {
+	 }
+		/** 
+	 * If teaser is empty the html-tag will be hidden on page 
+	 */
+	 function hide_teaser_on_page_if_empty() {
+	 	if ($(".node-type-ding-page .page-lead").is(':empty')) {
+	 		$(".node-type-ding-page .page-lead").hide();
+	 	}
+	 }
 
-	
-	  $(".bibsdb-toggle-fold").click(function() {
+	 function bibsdb_foldable_list() {
+
+
+	 	$(".bibsdb-toggle-fold").click(function() {
 	      //$(".bibsdb-foldable-field").hide();
 	      $(this).siblings(".bibsdb-foldable-field").slideToggle("fast");
-    });
-	}
-	
+	    });
+	 }
+
 	/** 
 	* Attach message to contact-page
-  */    
-  function attach_message_to_contact_page() {
- 
+	*/    
+	function attach_message_to_contact_page() {
+
     // only apply to contact page
     if (window.location.href.indexOf("/contact") == -1) {
-      return;
+    	return;
     }
 
     var message = $('<div>').addClass("bibsdb-contact-message").text("Spørgsmål og ønsker til bestillinger skal ske ved personlig eller telefonisk henvendelse.");
-   
+
     $(".primary-content .pane-content").prepend(message);
   }
 
@@ -44,10 +52,11 @@
 
   // When ready start the magic.
   $(document).ready(function () {
-		hide_page_taxonomy_tags();
-		bibsdb_foldable_list();
-		attach_message_to_contact_page();
-	});
+  	hide_page_taxonomy_tags();
+  	bibsdb_foldable_list();
+  	attach_message_to_contact_page();
+  	hide_teaser_on_page_if_empty();
+  });
 
 
 
